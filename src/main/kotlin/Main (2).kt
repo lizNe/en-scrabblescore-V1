bf5189e0cbeb9a879b128a15072965ab35c1b2ad
lizzz <20071724@mail.wit.ie>
2022-02-19 17:21:11 +0000

    Comments on main.kt done

--- src/main/kotlin/Main (2).kt
+++ src/main/kotlin/Main (2).kt
@@ -10,10 +10,15 @@ private val scrabbleScore = ScrabbleScore()
 //    - display the Scrabble Score for the word (the case of the letters shouldn't matter i.e. u is the same as U)
 //    - the user should be continually asked to enter a word until they type a sentinel value (e.g. -1) to end the program
 
+//Break ends and breaks the while loop
+//$word displays the word entered by the user. scrabbleScore is called to execute the class ScrabbleScore and within that class use the function scoreLetter.
+// The parameter word passed into .scoreWord(word)
+// Used if statement to end the program
+
 fun main() {
     println(scrabbleScore.letterValues())
     while (true) {
-        println("Enter a word and compute the Scrabble Score: ")
+        print("Enter a word and compute the Scrabble Score: ")
         val word = input.nextLine()
         println("$word has a value of ${scrabbleScore.scoreWord(word)}")
         if (word=="-1") {
